d94e7710e60ec3cf41a4a7affd0c00e733c0d8e8
Tyler <lorenarcdev@gmail.com>
2025-01-12 10:46:45 -0500

    Tranferred over the audio manager. To make it compatible, a new constructor parameter has been defined.

---
+++ include/sys_audio.h
@@ -0,0 +1,25 @@
+// sys_audio.h
+#pragma once
+#include <raylib.h>
+#include <string>
+#include <toml/value.hpp>
+#include <vector>
+#include <tuple>
+
+
+/* The Audio Manager where the game's sound effects are loaded and stored.
+ * The same goes for the game's music as well. This is all done through
+ * a usage of a TOML file located in the audio directory.*/
+class AudioManager {
+public:
+  AudioManager(std::string data_path);
+  ~AudioManager();
+
+  void loadSoundEffects();
+  void createMusicMetaData();
+  std::tuple<float, float> getPitchValues(toml::value sound_data);
+
+private:
+  toml::value meta_data;
+  std::vector<Sound> sound_effects;
+};
