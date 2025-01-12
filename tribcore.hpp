630b41648eb8874a4d7ec39c5f7ee39f42932962
Tyler <lorenarcdev@gmail.com>
2025-01-12 12:58:27 -0500

    Added more stuff to base header.

--- tribcore.hpp
+++ tribcore.hpp
@@ -2,8 +2,10 @@
 #ifndef TRIBCORE_HPP
 #define TRIBCORE_HPP
 
-#include "base/actor.h"
-#include "base/dynamic_actor.h"
+#include "include/base/actor.h"
+#include "include/base/dynamic_actor.h"
+#include "include/base/generics.h"
+#include "include/base/scene.h"
 
 #endif // !TRIBCORE_HPP
 
