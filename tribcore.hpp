e696c3ab30608e7898d2537ee0576deffee8f986
Tyler <lorenarcdev@gmail.com>
2025-01-12 13:03:25 -0500

    Adjusted include order

--- tribcore.hpp
+++ tribcore.hpp
@@ -2,10 +2,10 @@
 #ifndef TRIBCORE_HPP
 #define TRIBCORE_HPP
 
-#include "include/base/actor.h"
-#include "include/base/dynamic_actor.h"
 #include "include/base/generics.h"
 #include "include/base/scene.h"
+#include "include/base/actor.h"
+#include "include/base/dynamic_actor.h"
 
 #endif // !TRIBCORE_HPP
 
